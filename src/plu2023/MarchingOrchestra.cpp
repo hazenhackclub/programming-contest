9c07f3e6fc698081600b23765a1060a448dccada
Sam DePoule <samdepoule@gmail.com>
2023-02-12 12:47:34 -0800

    Finished programming MarchingOrchestra in cpp

--- src/plu2023/MarchingOrchestra.cpp
+++ src/plu2023/MarchingOrchestra.cpp
@@ -2,10 +2,101 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
 
 using namespace std;
 
-int closestDirection( pair<int, int> pos, pair<int, int> des ) {
+enum Dir { N, E, S, W };
+
+vector<Dir> closestDirection( pair<int, int> pos, pair<int, int> des ) {
+	
+	int dY = -(des.first - pos.first), dX = des.second - pos.second;
+	vector<Dir> arr{4};
+	if (abs(dX) > abs(dY)) {
+		if (dX > 0) {
+			arr[0] = E;
+			arr[3] = W;
+		} else {
+			arr[0] = W;
+			arr[3] = E;
+		}
+
+		if (dY > 0) {
+			arr[1] = N;
+			arr[2] = S;
+		} else {
+			arr[1] = S;
+			arr[2] = N;
+		}
+	} else {
+		if (dY > 0) {
+			arr[0] = N;
+			arr[3] = S;
+		} else {
+			arr[0] = S;
+			arr[3] = N;
+		}
+
+		if (dX > 0) {
+			arr[1] = E;
+			arr[2] = W;
+		} else {
+			arr[1] = W;
+			arr[2] = E;
+		}
+	}
+
+	return arr;
+}
+
+pair<int,  int> moveDirection(pair< int,  int> pos, Dir direction) {
+	switch (direction) {
+		default:
+		case N:
+			return pair<int, int>{pos.first - 1, pos.second};
+		case E:
+			return pair<int, int>{pos.first, pos.second + 1};
+		case S:
+			return pair<int, int>{pos.first + 1, pos.second};
+		case W:
+			return pair<int, int>{pos.first, pos.second - 1};
+	}
+}
+
+bool canMove(pair<int, int> pos, Dir direction, vector< vector<char> > map, vector< vector<bool> > visited) {
+	pair<int, int> newPos = moveDirection(pos, direction);
+	return newPos.first >= 0 && newPos.second >= 0 && newPos.first < map.size()
+			&& newPos.second < map[0].size() && map[newPos.first][newPos.second] != '#'
+			&& !visited[newPos.first][newPos.second];
+}
+
+
+
+// vector< vector<bool> > clone( vector< vector<bool> > clone ) {
+
+// 	vector< vector<bool> > dest[clone.size()];
+// 	// std::copy(clone, clone + clone.size(), back_inserter(dest));
+// 	std::copy(clone.begin(), clone.end(), back_inserter(dest));
+// 	return dest;
+// }
+
+int findShortestPath( pair<int, int> pos, pair<int, int> des, vector< vector<char> > map, vector< vector<bool> > visited, int length ) {
+
+	if( pos.first == des.first && pos.second == des.second )
+		return length;
+
+	visited[pos.first][pos.second] = true; // check off this square
+	vector<Dir> orderedDirections = closestDirection( pos, des );
+
+	for( Dir dir : orderedDirections ) {
+		if( canMove( pos, dir, map, visited ) ) {
+			int result = findShortestPath( moveDirection( pos, dir ), des, map, visited, length + 1 );
+			if( result != -1 )
+				return result;
+		}
+	}
+
+	return -1;
 }
 
 int main( ) {
@@ -22,20 +113,45 @@ int main( ) {
 		int n, m;
 		myFile >> n;
 		myFile >> m;
+
+		vector< vector<char> > maze; // y, x
+		vector< vector<bool> > visited; // y, x
+		map<char, pair<int, int> > startingPositions {};
+		map<char, pair<int, int> > endingPositions {};
+
 		for( int row = 0; row < n; row++ ) {
 			vector<char> line( m );
-			for( int column = 0; column < m; column++ )
+			for( int column = 0; column < m; column++ ){
 				myFile >> line[column];
 
-			// for( auto i : line )
-			// 	std::cout << i << ' ';
-			// cout << endl;
-		}
-		cout << endl;
+				int num = (int) line[column];
+				if( num >= 65 && num <= 90 )
+					startingPositions[line[column]] = pair<int, int>{ row, column } ;
+				else if( num >= 97 && num <= 122 )
+					endingPositions[line[column]] = pair<int, int>{ row, column } ;
+			}
 
-		// cout << n << " and m: " << m << endl;
+			maze.push_back(line);
+			visited.push_back(vector<bool>( m ));
+		}
+
+		for( auto row : maze ) {
+			for( char i : row )
+				std::cout << i << ' ';
+			cout << endl;
+		}
+
+		int length = 0;
+		for( map<char, pair<int, int> >::iterator itr = startingPositions.begin(); itr != startingPositions.end(); itr++){
+			int cur = findShortestPath( startingPositions.at( itr->first ), endingPositions.at( (char) tolower(itr->first) ), maze, visited, 0 );
+			length += cur;
+			cout << itr->first << " " << cur << endl;
+}
+		cout << length << endl;
 	}
 
 	myFile.close( );
+
 	return 0;
 }
+
--- src/plu2023/MarchingOrchestra.java
+++ src/plu2023/MarchingOrchestra.java
@@ -15,97 +15,94 @@ public class MarchingOrchestra {
 		N, E, S, W;
 	}
 
-	public static boolean found;
+	public static void main( String[] args ) throws FileNotFoundException {
+		Scanner scan = new Scanner( new File( "marching.dat" ) );
 
-	public static void main(String[] args) throws FileNotFoundException {
-		Scanner scan = new Scanner(new File("marching.dat"));
+		int numLines = Integer.parseInt( scan.nextLine( ) );
 
-		int numLines = Integer.parseInt(scan.nextLine());
-
-		for (int i = 0; i < numLines; i++) {
-			int n = scan.nextInt(), m = scan.nextInt();
-			scan.nextLine();
+		for( int i = 0; i < numLines; i++ ) {
+			int n = scan.nextInt( ), m = scan.nextInt( );
+			scan.nextLine( );
 			char[][] map = new char[n][m]; // y, x
 			boolean[][] visited = new boolean[n][m]; // y, x
-			HashMap<Character, Pair<Integer, Integer>> startingPositions = new HashMap<>();
-			HashMap<Character, Pair<Integer, Integer>> endingPositions = new HashMap<>();
+			HashMap<Character, Pair<Integer, Integer>> startingPositions = new HashMap<>( );
+			HashMap<Character, Pair<Integer, Integer>> endingPositions = new HashMap<>( );
 
-			for (int row = 0; row < n; row++) {
-				String line = scan.nextLine();
-				for (int column = 0; column < m; column++) {
-					map[row][column] = line.charAt(column);
+			for( int row = 0; row < n; row++ ) {
+				String line = scan.nextLine( );
+				for( int column = 0; column < m; column++ ) {
+					map[row][column] = line.charAt( column );
 					int num = map[row][column];
-					if (num >= 65 && num <= 90)
-						startingPositions.put(map[row][column], new Pair<Integer, Integer>(row, column));
-					else if (num >= 97 && num <= 122)
-						endingPositions.put(map[row][column], new Pair<Integer, Integer>(row, column));
+					if( num >= 65 && num <= 90 )
+						startingPositions.put( map[row][column], new Pair<Integer, Integer>( row, column ) );
+					else if( num >= 97 && num <= 122 )
+						endingPositions.put( map[row][column], new Pair<Integer, Integer>( row, column ) );
 				}
 			}
 
 			int length = 0;
-			for (Character key : startingPositions.keySet())
-				length += findShortestPath(startingPositions.get(key), endingPositions.get(Character.toLowerCase(key)),
-						map, clone(visited), 0);
+			for( Character key : startingPositions.keySet( ) )
+				length += findShortestPath( startingPositions.get( key ),
+						endingPositions.get( Character.toLowerCase( key ) ), map, clone( visited ), 0 );
 
-			System.out.println(length);
+			System.out.println( length );
 		}
 	}
 
-	public static int findShortestPath(Pair<Integer, Integer> pos, Pair<Integer, Integer> des, char[][] map,
-			boolean[][] visited, int length) {
+	public static int findShortestPath( Pair<Integer, Integer> pos, Pair<Integer, Integer> des, char[][] map,
+			boolean[][] visited, int length ) {
 
-		if (pos.getKey().equals(des.getKey()) && pos.getValue().equals(des.getValue()))
+		if( pos.getKey( ).equals( des.getKey( ) ) && pos.getValue( ).equals( des.getValue( ) ) )
 			return length;
 
-		visited[pos.getKey()][pos.getValue()] = true; // check off this square
-		Dir[] orderedDirections = closestDirection(pos, des);
+		visited[pos.getKey( )][pos.getValue( )] = true; // check off this square
+		Dir[] orderedDirections = closestDirection( pos, des );
 
-		for (Dir dir : orderedDirections) {
-			if (canMove(pos, dir, map, visited)) {
-				int result = findShortestPath(moveDirection(pos, dir), des, map, clone(visited), length + 1);
-				if (result != -1)
+		for( Dir dir : orderedDirections ) {
+			if( canMove( pos, dir, map, visited ) ) {
+				int result = findShortestPath( moveDirection( pos, dir ), des, map, clone( visited ), length + 1 );
+				if( result != -1 )
 					return result;
 			}
 		}
 
 		return -1;
-
 	}
 
-	public static boolean[][] clone(boolean[][] clone) {
+	public static boolean[][] clone( boolean[][] clone ) {
 		boolean[][] myArr = new boolean[clone.length][];
-		for (int i = 0; i < clone.length; i++)
-			myArr[i] = clone[i].clone();
+		for( int i = 0; i < clone.length; i++ )
+			myArr[i] = clone[i].clone( );
 		return myArr;
 	}
 
-	public static boolean canMove(Pair<Integer, Integer> pos, Dir direction, char[][] map, boolean[][] visited) {
-		Pair<Integer, Integer> newPos = moveDirection(pos, direction);
-		return newPos.getKey() >= 0 && newPos.getValue() >= 0 && newPos.getKey() < map.length
-				&& newPos.getValue() < map[0].length && map[newPos.getKey()][newPos.getValue()] != '#'
-				&& !visited[newPos.getKey()][newPos.getValue()];
+	public static boolean canMove( Pair<Integer, Integer> pos, Dir direction, char[][] map, boolean[][] visited ) {
+		Pair<Integer, Integer> newPos = moveDirection( pos, direction );
+		return newPos.getKey( ) >= 0 && newPos.getValue( ) >= 0 && newPos.getKey( ) < map.length
+				&& newPos.getValue( ) < map[0].length && map[newPos.getKey( )][newPos.getValue( )] != '#'
+				&& !visited[newPos.getKey( )][newPos.getValue( )];
 	}
 
-	public static Pair<Integer, Integer> moveDirection(Pair<Integer, Integer> pos, Dir direction) {
-		switch (direction) {
+	public static Pair<Integer, Integer> moveDirection( Pair<Integer, Integer> pos, Dir direction ) {
+		switch ( direction) {
 			default:
 			case N:
-				return new Pair<Integer, Integer>(pos.getKey() - 1, pos.getValue());
+				return new Pair<Integer, Integer>( pos.getKey( ) - 1, pos.getValue( ) );
 			case E:
-				return new Pair<Integer, Integer>(pos.getKey(), pos.getValue() + 1);
+				return new Pair<Integer, Integer>( pos.getKey( ), pos.getValue( ) + 1 );
 			case S:
-				return new Pair<Integer, Integer>(pos.getKey() + 1, pos.getValue());
+				return new Pair<Integer, Integer>( pos.getKey( ) + 1, pos.getValue( ) );
 			case W:
-				return new Pair<Integer, Integer>(pos.getKey(), pos.getValue() - 1);
+				return new Pair<Integer, Integer>( pos.getKey( ), pos.getValue( ) - 1 );
 		}
 	}
 
-	public static Dir[] closestDirection(Pair<Integer, Integer> pos, Pair<Integer, Integer> des) {
+	public static Dir[] closestDirection( Pair<Integer, Integer> pos, Pair<Integer, Integer> des ) {
 
-		int dY = -(des.getKey() - pos.getKey()), dX = des.getValue() - pos.getValue();
+		int dY = -( des.getKey( ) - pos.getKey( ) ), dX = des.getValue( ) - pos.getValue( );
 		Dir[] arr = new Dir[4];
-		if (Math.abs(dX) > Math.abs(dY)) {
-			if (dX > 0) {
+		if( Math.abs( dX ) > Math.abs( dY ) ) {
+			if( dX > 0 ) {
 				arr[0] = Dir.E;
 				arr[3] = Dir.W;
 			} else {
@@ -113,7 +110,7 @@ public class MarchingOrchestra {
 				arr[3] = Dir.E;
 			}
 
-			if (dY > 0) {
+			if( dY > 0 ) {
 				arr[1] = Dir.N;
 				arr[2] = Dir.S;
 			} else {
@@ -121,7 +118,7 @@ public class MarchingOrchestra {
 				arr[2] = Dir.N;
 			}
 		} else {
-			if (dY > 0) {
+			if( dY > 0 ) {
 				arr[0] = Dir.N;
 				arr[3] = Dir.S;
 			} else {
@@ -129,7 +126,7 @@ public class MarchingOrchestra {
 				arr[3] = Dir.N;
 			}
 
-			if (dX > 0) {
+			if( dX > 0 ) {
 				arr[1] = Dir.E;
 				arr[2] = Dir.W;
 			} else {
